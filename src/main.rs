616ca8adfd1f45fd359f29559a673af8246de54d
alxslash@newrzm.com <alxslash@newrzm.com>
2018-12-25 10:43:04 +0800

    init mingrep

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::env;
+use std::process;
+
+use mingrep;
+
+use mingrep::Config;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+
+        process::exit(1);
+    });
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+
+    if let Err(e) = mingrep::run(config) {
+        println!("Application error: {}", e);
+
+        process::exit(1);
+    };
+}
